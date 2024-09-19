17d40a8edcaafca2c003b02dcabbecd16dddf99c
ashutosh-gole <ashutoshsgole@gmail.com>
2024-09-19 12:43:43 +0530

    client and server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
       <Link href="/users">Users Page</Link>
+      <ProductCard />
     </main>
   );
 }
